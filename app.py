0ae41063e731d38598893303958cdaf646f8fea4
Mohammadreza Zakeri (Zaki) <31301756+ZAKI1905@users.noreply.github.com>
2025-03-06 16:07:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -60,13 +60,21 @@ def check_answer(set_number, I1, I2, I3):
 # Log Submission to Google Sheets via Apps Script
 def log_submission_to_apps_script(set_number, I1, I2, I3, result, name=""):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    gs_result = ""
+    if result.startswith("✅"):
+        gs_result = "Correct"
+    elif result.startswith("⚠️"):
+        gs_result = "Almost correct"
+    else:
+        gs_result = "Incorrect"
+
     payload = {
         "timestamp": timestamp,
         "set_number": set_number,
         "I1": I1,
         "I2": I2,
         "I3": I3,
-        "result": result.replace("\n", " "),
+        "result": gs_result,
         "name": name
     }
     requests.post(APPS_SCRIPT_URL, json=payload)
